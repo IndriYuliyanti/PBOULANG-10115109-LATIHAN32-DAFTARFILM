36051e2176c171497388b10de224998a973e7837
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-10-17 15:40:19 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan32-daftarfilm/src/pboulang/pkg10115109/latihan32/daftarfilm/Pboulang10115109Latihan32Daftarfilm.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan32.daftarfilm;
+
+/**
+ *
+ * @author nama : indri yuliyanti
+ *          nim : 10115109
+ *          kls : pbo ulang
+ *          tugas pert 3 : Daftar film
+ */
+public class Pboulang10115109Latihan32Daftarfilm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("===Daftar Film Sedang Tayang==="+"\n"+"");
+        System.out.println("Judul film \t\t: Venom");
+        System.out.println("Genre Film \t\t: Action,Horror,Scifi");
+        System.out.println("Rating Film \t\t: 8.5");
+        System.out.println("Duration Film\t\t: 120 Menit"+"\n"+"");
+        
+       
+        System.out.println("Judul film \t\t: Small foot");
+        System.out.println("Genre Film \t\t: Animation");
+        System.out.println("Rating Film \t\t: 9.0");
+        System.out.println("Duration Film\t\t: 90 Menit"+"\n"+"");
+        
+        
+        System.out.println("Judul film \t\t: Crazy rich asian");
+        System.out.println("Genre Film \t\t: Comedy");
+        System.out.println("Rating Film \t\t: 7.8");
+        System.out.println("Duration Film\t\t: 119 Menit"+"\n"+"");
+        
+        
+        System.out.println("Judul film \t\t: Asih");
+        System.out.println("Genre Film \t\t: Horror");
+        System.out.println("Rating Film \t\t: 6.0");
+        System.out.println("Duration Film\t\t: 100 Menit"+"\n"+"");
+        
+        
+    
+    
+    }
+    
+}
